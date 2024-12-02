340354f9b1500bc90770e712818fe1fcd1aa0e34
ncwanezama00 <zamangeyane@gmail.com>
2024-12-02 15:05:16 +0200

    rectify existing components

--- src/Components/HomePage/AdvertisementFour/AdvertisementFour.js
+++ src/Components/HomePage/AdvertisementFour/AdvertisementFour.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./AdvertisementFour.css";
 
 function AdvertisementFour(props){
     return(
--- src/Components/HomePage/MainPage.js
+++ src/Components/HomePage/MainPage.js
@@ -1,6 +1,7 @@
 import React, { Component} from 'react';
 import AdvertisementOne from './AdvertisementOne/AdvertisementOne';
 import "./MainPage.css"
+import AdvertisementFour from './AdvertisementFour/AdvertisementFour';
 class MainPage extends Component{
     constructor(props){
         super(props);
@@ -10,11 +11,14 @@ class MainPage extends Component{
         return(
          <div className='mainpage'>
             
-            <div style={{paddingTop:"260px", "display":"flex"}}>
+            <div style={{paddingTop:"260px", "display":"flex", flexWrap:"wrap"}}>
+                <AdvertisementOne/>
+                <AdvertisementFour/>
                 <AdvertisementOne/>
                 <AdvertisementOne/>
                 <AdvertisementOne/>
-                <AdvertisementOne/>
+                <AdvertisementFour/>
+                <AdvertisementFour/>
                 <AdvertisementOne/>
             </div>
          </div>   
